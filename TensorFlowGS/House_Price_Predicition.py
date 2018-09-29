56fad96a079e63c6c83f4b71b6094228120e366e
Breno Marques <breno@brolam.com.br>
2018-09-29 20:53:30 +0000

    feat: price vs. predicted price

--- TensorFlowGS/House_Price_Predicition.py
+++ TensorFlowGS/House_Price_Predicition.py
@@ -8,48 +8,141 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# generation some house sizes between 1000 and 3500 (typical sq ft of house)
+#  generation some house sizes between 1000 and 3500 (typical sq ft of house)
 num_house = 160
-num_seed = 42
-np.random.seed(num_seed)
-house_size = np.random.randint(low=1000, high=3500, size=num_house)
+np.random.seed(42)
+house_size = np.random.randint(low=1000, high=3500, size=num_house )
 
 # Generate house prices from house size with a random noise added.
-np.random.seed(num_seed)
-house_price = house_size * 100.0 + np.random.randint(low=20000, high=70000, size=num_house)
+np.random.seed(42)
+house_price = house_size * 100.0 + np.random.randint(low=20000, high=70000, size=num_house)  
 
-print(house_size)
-print(house_price)
+# Plot generated house and size 
+plt.plot(house_size, house_price, "bx")  # bx = blue x
+plt.ylabel("Price")
+plt.xlabel("Size")
+plt.savefig('display_house_price_01.svg') 
 
+# you need to normalize values to prevent under/overflows.
 def normalize(array):
-    return ( array - array.mean()) / array.std()
+    return (array - array.mean()) / array.std()
 
-# define number of training samples, 0,7 = 70%. We can take the first 70% since the values are randominze     
+def normalize_single_value(value, array):
+  return (value - array.mean()) / array.std()
+
+def denormalize(value, array):
+  return value * array.std() + array.mean()
+  
+# define number of training samples, 0.7 = 70%.  We can take the first 70% since the values are randomized
 num_train_samples = math.floor(num_house * 0.7)
-print('Amount Samples: {0} / {1}'.format(num_train_samples, num_house))
 
-#define training data
+# define training data
 train_house_size = np.asarray(house_size[:num_train_samples])
-train_house_price = np.asanyarray(house_price[:num_train_samples])
-print('Amount Samples Traing Size:{0} / Price:{1}'.format(train_house_size.size, train_house_price.size))
+train_price = np.asanyarray(house_price[:num_train_samples:])
 
 train_house_size_norm = normalize(train_house_size)
-train_house_price_norm = normalize(train_house_price)
+train_price_norm = normalize(train_price)
 
-print("Traing Size Normalize: ",train_house_size_norm)
-print("Traing Price Normalize", train_house_price_norm)
+# define test data
+test_house_size = np.array(house_size[num_train_samples:])
+test_house_price = np.array(house_price[num_train_samples:])
 
-# Set up the TensorFlow placeholders that get updated as we descend the gradient
+test_house_size_norm = normalize(test_house_size)
+test_house_price_norm = normalize(test_house_price)
+
+#  Set up the TensorFlow placeholders that get updated as we descend down the gradient
 tf_house_size = tf.placeholder("float", name="house_size")
-tf_house_price = tf.placeholder("float", name="house_price")
+tf_price = tf.placeholder("float", name="price")
 
-# Define the variables holding the size_factor and price we set during training.
+# Define the variables holding the size_factor and price we set during training.  
 # We initialize them to some random values based on the normal distribution.
 tf_size_factor = tf.Variable(np.random.randn(), name="size_factor")
-tf_price_offset = tf.Variable(np.random.randn(),"")
+tf_price_offset = tf.Variable(np.random.randn(), name="price_offset")
 
-#plt.plot(house_size, house_price, "bx")
-#plt.ylabel("Price")
-#plt.xlabel("Size")
-#plt.savefig('display.svg') 
 
+# 2. Define the operations for the predicting values - predicted price = (size_factor * house_size ) + price_offset
+#  Notice, the use of the tensorflow add and multiply functions.  These add the operations to the computation graph,
+#  AND the tensorflow methods understand how to deal with Tensors.  Therefore do not try to use numpy or other library 
+#  methods.
+tf_price_pred = tf.add(tf.multiply(tf_size_factor, tf_house_size), tf_price_offset)
+
+
+# 3. Define the Loss Function (how much error) - Mean squared error
+tf_cost = tf.reduce_sum(tf.pow(tf_price_pred-tf_price, 2))/(2*num_train_samples)
+
+
+# Optimizer learning rate.  The size of the steps down the gradient
+learning_rate = 0.1
+
+# 4. define a Gradient descent optimizer that will minimize the loss defined in the operation "cost".
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(tf_cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+# Launch the graph in the session
+with tf.Session() as sess:
+    sess.run(init)
+
+    # set how often to display training progress and number of training iterations
+    display_every = 2
+    num_training_iter = 50
+
+    # calculate the number of lines to animation
+    fit_num_plots = math.floor(num_training_iter/display_every)
+    # add storage of factor and offset values from each epoch
+    fit_size_factor = np.zeros(fit_num_plots)
+    fit_price_offsets = np.zeros(fit_num_plots)
+    fit_plot_idx = 0    
+
+   # keep iterating the training data
+    for iteration in range(num_training_iter):
+
+        # Fit all training data
+        for (size, price) in zip(train_house_size_norm, train_price_norm):
+            sess.run(optimizer, feed_dict={tf_house_size: size, tf_price: price})
+            value = normalize_single_value(size, house_size)
+            price_prediction = sess.run(tf_price_pred, feed_dict={tf_house_size:value})
+            price_prediction = denormalize(price_prediction, house_price )
+            print("House size:",round(size, 2), " Original price:", round(price,2) , "Price Prediction:", round(price_prediction, 2), "Diff:", (round(denormalize(price, house_price ) )) )
+
+
+        # Display current status
+        if (iteration + 1) % display_every == 0:
+            c = sess.run(tf_cost, feed_dict={tf_house_size: train_house_size_norm, tf_price:train_price_norm})
+            print("iteration #:", '%04d' % (iteration + 1), "cost=", "{:.9f}".format(c), \
+                "size_factor=", sess.run(tf_size_factor), "price_offset=", sess.run(tf_price_offset))
+            # Save the fit size_factor and price_offset to allow animation of learning process
+            fit_size_factor[fit_plot_idx] = sess.run(tf_size_factor)
+            fit_price_offsets[fit_plot_idx] = sess.run(tf_price_offset)
+            fit_plot_idx = fit_plot_idx + 1
+            
+    print("Optimization Finished!")
+    training_cost = sess.run(tf_cost, feed_dict={tf_house_size: train_house_size_norm, tf_price: train_price_norm})
+    print("Trained cost=", training_cost, "size_factor=", sess.run(tf_size_factor), "price_offset=", sess.run(tf_price_offset), '\n')
+
+
+    # Plot of training and test data, and learned regression
+    # get values used to normalized data so we can denormalize data back to its original scale
+    train_house_size_mean = train_house_size.mean()
+    train_house_size_std = train_house_size.std()
+
+    train_price_mean = train_price.mean()
+    train_price_std = train_price.std()
+
+    # Plot the graph
+    plt.rcParams["figure.figsize"] = (10,8)
+    plt.figure()
+    plt.ylabel("Price")
+    plt.xlabel("Size (sq.ft)")
+    plt.plot(train_house_size, train_price, 'go', label='Training data')
+    plt.plot(test_house_size, test_house_price, 'mo', label='Testing data')
+    plt.plot(train_house_size_norm * train_house_size_std + train_house_size_mean,
+             (sess.run(tf_size_factor) * train_house_size_norm + sess.run(tf_price_offset)) * train_price_std + train_price_mean,
+             label='Learned Regression')
+ 
+    #plt.legend(loc='upper left')
+    plt.savefig('display_house_price_02.svg')  
+    print("TensorFlow Rank:", tf.rank)
+    #print("TensorFlow Shape:", tf.get_shape())
+    
